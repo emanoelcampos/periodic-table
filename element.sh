d2115489a33d2fe04e39ddd36d382544db9a62c8
emanoel campos <emanoel.cv@gmail.com>
2024-03-27 05:09:14 +0000

    feat: add argument empty condition

--- element.sh
+++ element.sh
@@ -3,3 +3,8 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 echo -e "\n~~~ Periodic Table ~~~~\n"
+
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit
+fi
