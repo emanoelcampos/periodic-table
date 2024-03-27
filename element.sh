118cb2802e65f3d51c48a203023ce5e0446f4086
emanoel campos <emanoel.cv@gmail.com>
2024-03-27 05:18:38 +0000

    feat: add output element number found condition

--- element.sh
+++ element.sh
@@ -14,3 +14,7 @@ if [[ $1 =~ ^[0-9]+$ ]]; then
   if [[ -z $ELEMENT ]]; then
     echo "I could not find that element in the database."
   else
+    echo "$ELEMENT" | while IFS=" |" read ATOMIC_NUMBER SYMBOL NAME MASS MELTING BOILING TYPE; do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+    done
+  fi
