a72003a8673b092c3fda5d5ec2f738b58e2b5aec
emanoel campos <emanoel.cv@gmail.com>
2024-03-27 05:26:58 +0000

    fix: delete script title

--- element.sh
+++ element.sh
@@ -2,8 +2,6 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-echo -e "\n~~~ Periodic Table ~~~~\n"
-
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit
